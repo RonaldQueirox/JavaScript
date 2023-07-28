a09e9242921d1b29c58cb2b8aed56a16eb91d20d
RonaldQueirox <121915325+RonaldQueirox@users.noreply.github.com>
2023-07-28 20:54:50 -0300

    Update lessons

---
+++ Aulas_Js/Aula-07/js/main.js
@@ -0,0 +1,19 @@
+// Dom
+
+/*
+DOM = Document Object Model
+Arvore com elementos desde o Browser, a
+Janela do Browser, o Documento e cada
+elemento html que foram os conteudos.
+
+navigator
+window
+ location
+ document -> html -> head e body
+ history
+*/
+
+// PARA MANIPULAR O DOM PRECISAMOS
+// indicar qual objeto usar, depois
+// chamar um método(função) para
+// selecionar ele, seus dados, trazer informações, etc.
