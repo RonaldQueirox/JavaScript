927374ef168fcd1c61e047e621509d414e336660
RonaldQueirox <121915325+RonaldQueirox@users.noreply.github.com>
2023-07-29 23:04:41 -0300

    Update lessons

--- Aulas_Js/Aula-07/js/main.js
+++ Aulas_Js/Aula-07/js/main.js
@@ -1,4 +1,4 @@
-// Dom
+// Dom -> HTML
 
 /*
 DOM = Document Object Model
@@ -17,3 +17,47 @@ window
 // indicar qual objeto usar, depois
 // chamar um método(função) para
 // selecionar ele, seus dados, trazer informações, etc.
+
+// .querySelector()
+// selecionar elemento com base na tag, #id ou .class
+let titulo = document.querySelector('#titulo')
+console.log(titulo)
+
+// .textContent
+// propriedade ou atributo textContent
+// serve para acessar ou alterar conteudo
+// de um elemento que foi selecionado
+console.log(titulo.textContent);
+console.log(titulo.innerHTML);
+
+titulo.textContent = 'DOM'
+
+// .querySelectorAll()
+// selecionar todos elemento com base na tag, #id ou .class
+let testando = document.querySelectorAll('.box');
+console.log(testando);
+document.write(testando[0].textContent);
+document.write(testando[1].textContent);
+document.write(testando[2].textContent);
+//testando[0].textContent = 'texto qualquer';
+
+// Existem também os métodos
+// MAS, PREFIRA USAR O .querySelector()
+/*
+.getElementsByTagName()
+.getElementById()
+.getElementByClassName()
+*/
+
+let teste_tag = document.getElementsByTagName('div');
+//console.log(teste_tag);
+//document.write(teste_tag[0].textContent);
+//document.write(teste_tag[1].textContent);
+
+let teste_id = document.getElementById('titulo');
+//console.log(teste_id);
+
+let teste_class = document.getElementsByClassName('box');
+//console.log(teste_class);
+//document.write(teste_class[0].textContent);
+//document.write(teste_class[1].textContent);
---
+++ Aulas_Js/Aula-11/js/script.js
@@ -0,0 +1,9 @@
+// Condicionais e de Escolha
+
+let sit1 = document.querySelector('#sit1');
+let sit2 = document.querySelector('#sit2');
+let sit3 = document.querySelector('#sit3');
+let escolha = document.querySelector('#escolha');
+
+let nota_final1 = 3;
+let nota_final2 = 8;
